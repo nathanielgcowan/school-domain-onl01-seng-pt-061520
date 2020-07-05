b96b259408b36499d566e20c38c9d68b8a32c1c5
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-07-05 23:04:52 +0000

    Automatically backed up by Learn

---
+++ lib/school.rb
@@ -0,0 +1,22 @@
+class School
+	attr_accessor :roster, :name, :grade 
+	attr_reader :school 
+	
+	def initialize(school)
+    @school = school
+  end
+  
+  def roster
+  	roster = {}
+  end 
+  
+  def add_student(name,grade)
+  	roster[grade] = []
+  	roster[grade] << name
+  		
+  end
+
+end
+
+
+School.new("R").roster.add_student("john",9)
