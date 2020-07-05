4dcb4ee8a7910ea5130688883780b0bdaab9192c
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-07-05 23:06:53 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,17 +1,5 @@
 class School
-  def initialize(name)
-    @name = name
-    @roster = {}
-  end
-
-  def roster
-    @roster
-  end
-
-  def add_student(name, grade)
-    if @roster[grade] != nil
-      @roster[grade] << name
-    else
-      @roster[grade] = [name]
-    end
-  end
+  
+  
+  
+end
