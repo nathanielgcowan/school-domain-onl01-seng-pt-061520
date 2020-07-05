2a4c890cc273a23d40984823f48b0f73194640fc
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-07-05 23:04:32 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,19 +1,3 @@
 class School
-  attr_accessor :name, :roster, :grade
-  attr_reader :school
   
-  def initialize(school)
-    @school = school
-  end
-  
-  def roster
-    roster={}
-  end
-  
-  def add_student (name,grade)
-    roster["grade"] = []
-    roster["grade"] << "name"
-  end
 end
-
-school.sort
