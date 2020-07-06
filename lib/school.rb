90e646fe07fb73fbd1424ecfa3b2746c42c9a2b7
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-07-06 02:01:09 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,13 +1,28 @@
 class School
-  
-  def grade(grade)
-    self.roster[grade]
+  def initialize(name)
+    @name = name
+    @roster = {}
   end
-  
-  def sort
-    self.roster.each do |grade, students|
-      students.sort!
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    if @roster[grade] != nil
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
     end
   end
-  
-end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |key, value|
+      value.sort!
+    end
+  end
+end 
